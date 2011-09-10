2e6a505594c6ad346a70e7fea9dc6ab46231ec27
fakedarren <darren.waddell@gmail.com>
2011-09-10 12:21:12 +0100

    Adding Geshi and Markdown+

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
 error_reporting(E_ALL); 
 ini_set("display_errors", 1); 
-include("application/classes/control.php");
+include("application/lib/control.php");
 
 Control::config("controllers_folder", "application/controllers");
 Control::config("views_folder", "application/views");
