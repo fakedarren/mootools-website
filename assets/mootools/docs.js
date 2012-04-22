bb8d1022138ec8aa8a2b8ca50b8b4337930a1747
fakedarren <darren.waddell@gmail.com>
2012-04-22 16:42:41 +0100

    Placeholders for new documentation pages (prime)

--- assets/mootools/docs.js
+++ assets/mootools/docs.js
@@ -1,6 +1,6 @@
 mootools.$(window).on('load', function domready(){
 
-	mootools.$('.expand-this span').on('click', function(){
+	mootools.$('.expand-this span, .collapse-this span').on('click', function(){
 		var className = this[0].parentNode.className;
 		this[0].parentNode.className = (className == 'expand-this' ? 'collapse-this' : 'expand-this');
 		this[0].innerText = (className == 'expand-this' ? '-' : '+');
