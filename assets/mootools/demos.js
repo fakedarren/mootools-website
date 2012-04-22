da5c9ad0f42ab5215676311e81e2b11b7bb404bb
fakedarren <darren.waddell@gmail.com>
2012-04-22 18:49:17 +0100

    Array.forEach()

--- assets/mootools/demos.js
+++ assets/mootools/demos.js
@@ -1,18 +1,22 @@
 mootools.$(window).on('load', function domready(){
-
-	mootools.$('.demo').forEach(function(demo){
+	
+	var demos = mootools.$('.demo');
+	
+	if (demos){
+		demos.forEach(function(demo){
 		
-		var tabs = mootools.$('.demo-tabs li', demo);
-		var content = mootools.$('.demo-content', demo);
+			var tabs = mootools.$('.demo-tabs li', demo);
+			var content = mootools.$('.demo-content', demo);
 		
-		tabs.forEach(function(tab, index){
-			mootools.$(tab).on('click', function(){
-				tabs.removeClass('active');
-				content.removeClass('active');
-				mootools.$(this).addClass('active');
-				mootools.$(content[index]).addClass('active');
+			tabs.forEach(function(tab, index){
+				mootools.$(tab).on('click', function(){
+					tabs.removeClass('active');
+					content.removeClass('active');
+					mootools.$(this).addClass('active');
+					mootools.$(content[index]).addClass('active');
+				});
 			});
 		});
-	});
+	}
 
 });
