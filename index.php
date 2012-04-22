f4b4f531e075813db124ebdb078becab03838c9a
fakedarren <darren.waddell@gmail.com>
2012-04-22 12:48:17 +0100

    Super-simple version switching

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 error_reporting(E_ALL); 
 ini_set("display_errors", 1);
 
-$version = '1.4.0';
-
 include("application/lib/control.php");
 include("application/lib/markdown.php");
 
 Control::config("controllers_folder", "application/controllers");
 Control::config("views_folder", "application/views");
 
-Control::route("/^docs/", "docs");
-Control::route("/^learn/", "learn");
-Control::route("/^download/", "download");
-Control::route("/^plugins/", "plugins");
-Control::route("/^blog/", "blog");
-Control::route("/^community/", "community");
+Control::config("version", isset($_GET['version']) ? $_GET['version'] : '2.0');
+
+Control::route("/^[0-9.]+?\/?docs/", "docs");
+Control::route("/^[0-9.]+?\/?learn/", "learn");
+Control::route("/^[0-9.]+?\/?download/", "download");
+Control::route("/^[0-9.]+?\/?plugins/", "plugins");
+Control::route("/^[0-9.]+?\/?blog/", "blog");
+Control::route("/^[0-9.]+?\/?community/", "community");
 
 new Control();
 ?>
