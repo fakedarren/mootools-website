afd76ddf9df42e84b838ccc2d1c39d397b895186
fakedarren <darren.waddell@gmail.com>
2012-04-23 18:46:14 +0100

    Demo Runner

---
+++ learning/demo-runner.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+$demo = $_GET['demo'];
+?>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Demo</title>
+	<style>
+		html, body {
+			font: 12px/1 Helvetica, Arial, sans-serif;
+			height: 0;
+			padding: 0;
+		}
+	</style>
+	<link href="/learning/demos/<?=$demo?>/demo.css" rel="stylesheet"/>
+</head>
+<body>
+<?=file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/learning/demos/' . $demo . '/demo.html')?>
+
+</body>
+<script src="/learning/demos/<?=$demo?>/demo.js"></script>
+</html>
