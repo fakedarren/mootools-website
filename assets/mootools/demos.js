698ad5ba1b83d692eeabbfb3b768ec0327c925b1
fakedarren <darren.waddell@gmail.com>
2012-04-29 16:12:36 +0100

    Fixing JS error

--- assets/mootools/demos.js
+++ assets/mootools/demos.js
@@ -1,20 +1,24 @@
 (function($){
 	
 	$(window).on('load', function(){
-	
-		$('.demo').handle(function(demo){
-			var tabs = $('.demo-tabs li', demo);
-			var content = $('.demo-content', demo);
+		
+		var demos = $('.demo');
+		
+		if (demos){
+			$('.demo').handle(function(demo){
+				var tabs = $('.demo-tabs li', demo);
+				var content = $('.demo-content', demo);
 
-			tabs.forEach(function(tab, index){
-				mootools.$(tab).on('click', function(){
-					tabs.removeClass('active');
-					content.removeClass('active');
-					$(this).addClass('active');
-					$(content[index]).addClass('active');
+				tabs.forEach(function(tab, index){
+					mootools.$(tab).on('click', function(){
+						tabs.removeClass('active');
+						content.removeClass('active');
+						$(this).addClass('active');
+						$(content[index]).addClass('active');
+					});
 				});
 			});
-		});
+		}
 	
 	});
 	
